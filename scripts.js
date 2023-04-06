3e2479911b93296f9fff49f2341e7dd592b02176
DimitrusB <dimitrusboss@mail.ru>
2023-04-06 21:28:27 +0300

    js

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// while(true) {
+//         const msg = prompt('Введите ваше сообщение'); 
+//         console.log(msg); 
+//         if (msg === 'continue') {
+//             continue;
+//         }
+//         if (msg === 'break') {
+//             break;
+//         }
+//     }
