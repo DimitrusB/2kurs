79eb993f95b290c7bbb66c93917aef0358f38ada
DimitrusB <dimitrusboss@mail.ru>
2023-04-13 23:16:43 +0300

    #

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-const timer = (deadline) => {
-    if (isNaN(+deadline)) { // + - приводим значение к number, если это NaN,
-        console.log('Передано некорректное число'); // выводим сообщение
-        return; // выходим из функции
-    }
 
-    let time = deadline;
-    const interval = setInterval(() => {
-        time -= 1;
-        console.log(time);
-    }, 1000);
+// ================================================
+// const timer = (deadline) => {
+//     if (isNaN(+deadline)) { // + - приводим значение к number, если это NaN,
+//         console.log('Передано некорректное число'); // выводим сообщение
+//         return; // выходим из функции
+//     }
 
-    setTimeout(() => {
-        clearInterval(interval);
-        console.log('Время истекло!')
-    }, deadline * 1000)
-};
+//     let time = deadline;
+//     const interval = setInterval(() => {
+//         time -= 1;
+//         console.log(time);
+//     }, 1000);
 
-const deadline = prompt('На сколько секунд вы хотите поставить таймер?');
-timer(deadline);
+//     setTimeout(() => {
+//         clearInterval(interval);
+//         console.log('Время истекло!')
+//     }, deadline * 1000)
+// };
+
+// const deadline = prompt('На сколько секунд вы хотите поставить таймер?');
+// timer(deadline);
