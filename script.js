d00daa6edfee5b7473054149b3d2aff6cfc40d12
dmitriy <admin@silverboats.ru>
2023-04-20 11:12:32 +0300

    3

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+// =============================#3
+// самый оптимальный способ получения массива из строки (на мой взягляд) - это с помощью метода split(), есть несколько способ в зависимости от желаемого результата
+// const str = 'Lorem ipsum dolor sit amet ? consectetur adipisicing elit. Illum esse,';
+// const arr = str.split('');
+// console.log(arr); //['L', 'o', 'r', 'e', 'm', ' ', 'i', 'p', 's', 'u', 'm', ' ', 'd', 'o', 'l', 'o', 'r', ' ', 's', 'i', 't', ' ', 'a', 'm', 'e', 't', ' ', '?', ' ', 'c', 'o', 'n', 's', 'e', 'c', 't', 'e', 't', 'u', 'r', ' ', 'a', 'd', 'i', 'p', 'i', 's', 'i', 'c', 'i', 'n', 'g', ' ', 'e', 'l', 'i', 't', '.', ' ', 'I', 'l', 'l', 'u', 'm', ' ', 'e', 's', 's', 'e', ','] в данном примере мы получим массив множества эллементов, для каждого симовола строки.
+
+
+// const str = 'Lorem ipsum dolor sit amet ? consectetur adipisicing elit. Illum esse,';
+// const arr = str.split(' '); //тут мы указали разделитель для массива
+// console.log(arr);
+// // ['Lorem', 'ipsum', 'dolor', 'sit', 'amet', '?', 'consectetur', 'adipisicing', 'elit.', 'Illum', 'esse,']
+
+// const str = 'Lorem ipsum dolor sit amet ? consectetur adipisicing elit. Illum esse,';
+// const arr = str.split(' ', 5); //тут используем лимит равный 5
+// console.log(arr);
+// // ['Lorem', 'ipsum', 'dolor', 'sit', 'amet']
+
+// const str = 'Lorem ipsum! dolor sit amet ? consectetur adipisicing elit. Illum esse,';
+// const arr = str.split(/[?,.,,,!]/);
+// console.log(arr);
+// // ['Lorem ipsum', ' dolor sit amet ', ' consectetur adipisicing elit', ' Illum esse', '']
+
+// ТЕПЕРЬ (На мой взгляд!!!) менеее оптимальный варианты
+
+// const str = 'Lorem ipsum dolor sit amet ? consectetur adipisicing elit. Illum esse,';
+// const arr=[]; //создаем пустой массив для дальнейшей работы с ним
+// for (const el of str) { //перебираем с пмощью цикла for..of строку
+//    }
+//    arr.push(str[]); //заносим в массим элемент по индексу
+//    arr.push(...str); // можно с помощью rest оператора передать все символы в массив 
+//    console.log(arr);
+
 
 // =============================#4
 // const numbers = [1, 2, 3, 4, 5];
